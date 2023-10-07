5dd1d66db58846faf67b7b4ce7fbc7ac48e80aea
navneet17360 <navneetanand.2021cs@technonjr.org>
2023-10-07 11:53:03 +0530

    codes learned

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log('Express Tutorial')
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  // console.log(req.method)4
+  const url = req.url;
+  //homepage
+  if (url === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>home page</h1>");
+    res.end();
+  }
+  //about page
+  else if (url === "/about") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>about page</h1>");
+    res.end();
+  }
+  //404 code use
+  else {
+    res.writeHead(404, { "content-type": "text/html" });
+    res.write("<h1>page not found</h1>");
+    res.end();
+  }
+});
+
+server.listen(5000);
